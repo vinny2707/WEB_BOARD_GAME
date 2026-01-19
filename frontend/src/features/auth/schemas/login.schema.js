104989231ff3b34d3d5fdfbeb16fccbab4b17be5
23120409 <khacvuong2707@gmail.com>
2026-01-19 18:48:18 +0700

    fix(fe): add input validation for register

--- frontend/src/features/auth/schemas/login.schema.js
+++ frontend/src/features/auth/schemas/login.schema.js
@@ -1,6 +1,6 @@
-import {z} from 'zod'
+import { z } from "zod";
 
 export const loginSchema = z.object({
-  username: z.string().min(1, 'Username is required'),
-  password: z.string().min(6, 'Password must be at least 6 characters long'),
-})
+  username: z.string().trim().min(1, "Username is required"),
+  password: z.string().min(1, "Password is required"),
+});
